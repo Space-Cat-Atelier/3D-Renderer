93c4e0969b7d67d03549cc12cdaa90d23c6266a5
Space Cat Atelier <139983936+Space-Cat-Atelier@users.noreply.github.com>
2024-07-05 00:02:32 +0500

    Add files via upload

--- 3D Renderer/3D Renderer.py
+++ 3D Renderer/3D Renderer.py
@@ -31,10 +31,12 @@ def draw_grid(pos, area, size):
             pygame.draw.rect(screen, PEACOCK, pygame.Rect(x, y, size, size), 1)
 
 cam_speed = 5
+turn_speed = 0.1
 
 class Camera():
     def __init__(self, x, y, z, z_offset, fov):
         self.pos = [x, y, z]
+        self.rota = [0, 0, 0]
         self.fov = fov
         self.z_offset = z_offset
 
@@ -42,6 +44,20 @@ class Camera():
         self.pos[0] += vec[0]
         self.pos[1] += vec[1]
         self.pos[2] += vec[2]
+
+    def pitch(self, angle):
+        self.rota[0] += angle
+
+    def yaw(self, angle):
+        self.rota[1] += angle
+
+    def roll(self, angle):
+        self.rota[2] += angle
+
+    def rotate(self, rotate):
+        self.pitch(rotate[0])
+        self.yaw(rotate[1])
+        self.roll(rotate[2])
 
 class Cuboid():
     def __init__(self, x, y, z, w, h, d):
@@ -70,11 +86,37 @@ class Cuboid():
         pygame.draw.circle(screen, WHITE, [pos[0], pos[1]], 7)
 
     def draw_edge(self, pos1, pos2): 
-        pygame.draw.line(screen, LBLUE, pos1, pos2, 7)
+        pygame.draw.line(screen, BLACK, pos1, pos2, 7)
+
+    def pitch(self, pos, angle):
+        y = pos[1]
+        z = pos[2]
+        pos[1] = y*cos(angle) - z*sin(angle)
+        pos[2] = y*sin(angle) + z*cos(angle)
+
+    def yaw(self, pos, angle):
+        x = pos[0]
+        z = pos[2]
+        pos[0] = x*cos(angle) + z*sin(angle)
+        pos[2] = z*cos(angle) - x*sin(angle)
+
+    def roll(self, pos, angle):
+        x = pos[0]
+        y = pos[1]
+        pos[0] = x*cos(angle) - y*sin(angle)
+        pos[1] = x*sin(angle) + y*cos(angle)
 
     def project(self, camera):
         pos_prj = []
+        rot_points = []
         for i in self.points:
+            point = i.copy()
+            self.pitch(point, camera.rota[0])
+            self.yaw(point, camera.rota[1])
+            self.roll(point, camera.rota[2])
+            rot_points.append(point)
+
+        for i in rot_points:
             x = i[0] - camera.pos[0]
             y = i[1] - camera.pos[1]
             z = i[2] - camera.pos[2]
@@ -93,10 +135,10 @@ class Cuboid():
             if pos_prj[i[0]] and pos_prj[i[1]]:
                 self.draw_edge(pos_prj[i[0]], pos_prj[i[1]])
 
-cube = Cuboid(-50, -50, -50, 25, 100, 300)
-cube2 = Cuboid(-50, -50, 225, 350, 100, 25)
-cube3 = Cuboid(275, -50, -50, 25, 100, 300)
-shapes = [cube, cube2, cube3]
+cube1 = Cuboid(-50, -50, -50, 25, 100, 300)
+cube2 = Cuboid(-50, -50, 225, 300, 100, 25)
+cube3 = Cuboid(225, -50, -50, 25, 100, 300)
+shapes = [cube1, cube2, cube3]
 
 cam = Camera(0, 0, 0, 200, w/2)
 
@@ -119,6 +161,11 @@ while run: #Game loop
         cam.move([0, 0, cam_speed])
     elif keys[pygame.K_DOWN]:
         cam.move([0, 0, -cam_speed])
+
+    if keys[pygame.K_LEFT]:
+        cam.yaw(turn_speed)
+    elif keys[pygame.K_RIGHT]:
+        cam.yaw(-turn_speed)
 
     screen.fill(DDBLUE)
     draw_grid([0, 0], size, 50)
